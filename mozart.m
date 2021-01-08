d8bf8bbf4e23e9751c76602c1bc973c1622d2cf5
Saran Sankar <31500520+saran-sankar@users.noreply.github.com>
2021-01-08 18:28:30 +0530

    Add files via upload

--- mozart.m
+++ mozart.m
@@ -3,48 +3,61 @@ clc;
 filename = 'Mozart-symphony-40.wav';
 
 [y,Fs] = audioread(filename);
-
-samples = [1,25*Fs];
+duration = audioinfo(filename).Duration;
+total_length = Fs*duration;
+samples = [1,total_length];
 
 [z,Fs] = audioread(filename,samples);
 
-%play 25s
+%play
 %sound(z,Fs);
 
-start = 60000;
-length = 100000;
+%initialize
+start = 1;
+length = total_length/2;
 
-%original sample
-f = z(start:start+length,1);
+while 1
+    
+    %original sample
+    f = z(start:start+length-1,1);
 
-corr = corr_fft(f,f);
-%display(sum(corr));
+    %corr = corr_fft(f,f);
+    %display(sum(corr));
 
-%plot(corr, '.');
-max_corr = 0;
-pos = 0;
+    %plot(corr, '.');
+    max_corr = 0;
+    pos = 1;
 
-for i=200000:1000:1000000
-    g = z(i:i+length,1);
-    corr = corr_fft(f,g);
-    if sum(corr) > max_corr
-        max_corr = sum(corr);
-        pos = i;
+    for i=start+length:220500:total_length-length-1
+        g = z(i:i+length-1,1);
+        corr = corr_fft(f,g);
+        if sum(corr) > max_corr
+            max_corr = sum(corr);
+            pos = i; 
+        end
     end
+
+    %discovered sample
+    g = z(pos:pos+length-1,1);
+    
+    if max_corr>1e+10
+        break;
+    end
+    
+    length=length-220500;
+    
 end
 
-%discovered sample
-g = z(pos:pos+length,1);
-
-plot(corr, '.')
-hold on;
-plot(xcorr(f,g));
+%plot(0)
+%plot(corr, '.')
+%hold on;
+%plot(xcorr(f,g));
 
 %play the orignal and discovered samples 
 %sound(f,Fs);
 %sound(g,Fs);
 
-display(sum(corr)); 
+%display(sum(corr));
 
 function corr = corr_fft(f,g)
     F = fft(f);
